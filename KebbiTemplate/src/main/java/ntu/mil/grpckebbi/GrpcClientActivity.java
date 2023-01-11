c290eb77b8d315e80ea2dcf07384d23bef073d73
Yu-Wei Liu <54787524+liuyuweitarek@users.noreply.github.com>
2023-01-11 21:20:52 +0800

    feat: voice_recorder and real time chart

--- KebbiTemplate/src/main/java/ntu/mil/grpckebbi/GrpcClientActivity.java
+++ KebbiTemplate/src/main/java/ntu/mil/grpckebbi/GrpcClientActivity.java
@@ -6,6 +6,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.media.MediaActionSound;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
@@ -15,6 +17,15 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.google.gson.Gson;
 import com.interaction.robot.interaction.InteractGrpc;
 import com.interaction.robot.interaction.RobotConnectReply;
@@ -49,6 +60,7 @@ import ntu.mil.grpckebbi.Vision.DetectService;
 import ntu.mil.grpckebbi.Vision.VideoRecordListener;
 import ntu.mil.grpckebbi.Vision.VideoStreamService;
 import ntu.mil.grpckebbi.Voice.SpeakUtil;
+import ntu.mil.grpckebbi.Voice.VoiceRecorder;
 
 public class GrpcClientActivity extends Activity{
     public static final String TAG = GrpcClientActivity.class.getSimpleName();
@@ -61,6 +73,11 @@ public class GrpcClientActivity extends Activity{
     private static GrpcClientActivity mGrpcContext;
     public static InteractGrpc.InteractBlockingStub mInteracter;
 
+    /** This params are for Demo: VoiceRecorder+GoogleS2T. */
+    public TextView txtDbValue, txtRecordFileName;
+    private LineChart mLineChart;
+    private MediaActionSound mMediaActionSound;
+    private VoiceRecorder mVoiceRecorder;
 
     /** Services State Params */
     private static boolean serviceActive = false;
@@ -126,11 +143,95 @@ public class GrpcClientActivity extends Activity{
         connectBtn.setOnClickListener(v -> {
             Connect(ip);
             setContentView(R.layout.voice_s2t); // DEMO_ADD
+            SetupVoiceRecorderGoogleS2TUI();
 //            mRobotAPI.showFace(); DEMO_COMMENT
         });
 
         localIp = findViewById(R.id.ipAdress);
         localIp.setText(GetLocalIpAddress());
+    }
+
+    /** These  functions are for Demo: VoiceRecorder+GoogleS2T. */
+    private void SetupVoiceRecorderGoogleS2TUI(){
+        txtDbValue = (TextView) findViewById(R.id.txtDbValue);
+        txtRecordFileName = findViewById(R.id.txtRecordFileName);
+
+        mLineChart = findViewById(R.id.liveChart);
+
+        mVoiceRecorder = new VoiceRecorder(this, voiceCallback);
+        mMediaActionSound = new MediaActionSound();
+        initChart();
+    }
+
+    private void initChart(){
+        mLineChart.getDescription().setEnabled(false);// Tag
+        mLineChart.setTouchEnabled(true);// Touchable
+        mLineChart.setDragEnabled(true);// Interactive
+
+        // Set a basic line
+        LineData data = new LineData();
+        data.setValueTextColor(Color.BLACK);
+        mLineChart.setData(data);
+
+        // Bottom left tags
+        Legend l =  mLineChart.getLegend();
+        l.setForm(Legend.LegendForm.LINE);
+        l.setTextColor(Color.BLACK);
+
+        // X Axis
+        XAxis x =  mLineChart.getXAxis();
+        x.setTextColor(Color.BLACK);
+        x.setDrawGridLines(true);//畫X軸線
+        x.setPosition(XAxis.XAxisPosition.BOTTOM);//把標籤放底部
+        x.setLabelCount(5,true);//設置顯示5個標籤
+
+        // Content of X axis
+        x.setValueFormatter(new ValueFormatter() {
+            @Override
+            public String getFormattedValue(float value) {
+                return "No. "+Math.round(value);
+            }
+        });
+
+        YAxis y = mLineChart.getAxisLeft();
+        y.setTextColor(Color.BLACK);
+        y.setDrawGridLines(true);
+        y.setAxisMaximum(16000);
+        y.setAxisMinimum(0);
+        mLineChart.getAxisRight().setEnabled(false);// Right YAxis invisible
+        mLineChart.setVisibleXRange(0,100);// Set visible range
+    }
+
+    private void addData(int inputData){
+        LineData data =  mLineChart.getData();
+
+        // DB data only, set to 0. If other type of data be added, set to other int.
+        ILineDataSet set = data.getDataSetByIndex(0);
+        if (set == null){
+            set = createSet("DB_DATA");
+            data.addDataSet(set);
+        }
+        data.addEntry(new Entry(set.getEntryCount(),inputData),0);
+
+        // Renew plot
+        data.notifyDataChanged();
+        mLineChart.notifyDataSetChanged();
+        mLineChart.setVisibleXRange(0,100); // Visible range
+        mLineChart.moveViewToX(data.getEntryCount());// Whether track on the newest data point
+    }
+
+    private LineDataSet createSet(String SetName) {
+        LineDataSet set = new LineDataSet(null, SetName);
+        set.setAxisDependency(YAxis.AxisDependency.LEFT);
+        set.setColor(Color.GRAY);
+        set.setLineWidth(2);
+        set.setDrawCircles(false);
+        set.setFillColor(Color.RED);
+        set.setFillAlpha(50);
+        set.setDrawFilled(true);
+        set.setValueTextColor(Color.BLACK);
+        set.setDrawValues(false);
+        return set;
     }
 
     private void Connect(String saved_ip) {
@@ -251,6 +352,7 @@ public class GrpcClientActivity extends Activity{
                 mSpeakTool.say(robotCommand.getValue());
                 break;
             case "listen":
+                startVoiceRecorder();
 //                mSpeakTool.sayThenListen(robotCommand.getValue());
                 /**
                                      Here is going to change listen source from Kebbi origin listen method into `Voice Recorder` + `Google S2T API ` to implement continue listening method.
@@ -386,4 +488,62 @@ public class GrpcClientActivity extends Activity{
     }
 
     /** Voice Service */
+    private final VoiceRecorder.Callback voiceCallback = new VoiceRecorder.Callback() {
+
+        private String byteToDbValue(byte[] buffer, int size){
+            for (int i = 0; i < size - 1; i += 2) {
+                // The buffer has LINEAR16 in little endian.
+                int s = buffer[i + 1];
+                if (s < 0) s *= -1;
+                s <<= 8;
+                s += Math.abs(buffer[i]);
+                return Integer.toString(s);
+            }
+            return "";
+        }
+
+        @Override
+        public void onVoiceStart(final String recordFilename) {
+            Log.d(TAG, "onVoiceStart()");
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    txtRecordFileName.setText(recordFilename);
+                }
+            });
+        }
+
+        @Override
+        public void onVoice(final byte[] data, final int size, boolean sentenceCompleted) {
+            Log.d(TAG, "onVoice()");
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    String db_value = byteToDbValue(data,size);
+                    addData(Integer.valueOf(db_value));
+                    txtDbValue.setText(db_value);
+                }
+            });
+        }
+
+        @Override
+        public void onVoiceEnd() {
+            Log.d(TAG, "onVoiceEnd()");
+            stopVoiceRecorder();
+        }
+    };
+
+    private void startVoiceRecorder(){
+        if(mVoiceRecorder != null){
+            mMediaActionSound.play(MediaActionSound.START_VIDEO_RECORDING);
+            mVoiceRecorder.start();
+        }
+    }
+
+    private void stopVoiceRecorder(){
+        if(mVoiceRecorder != null){
+            mMediaActionSound.play(MediaActionSound.STOP_VIDEO_RECORDING);
+            mVoiceRecorder.stop();
+        }
+    }
 }
